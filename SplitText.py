c7ae5e361a85936571f77e50bdb86b853f592382
Anthony Kerr <46275713+PoeticDeath@users.noreply.github.com>
2024-07-25 02:07:50 -0400

    V1.0.2

--- SplitText.py
+++ SplitText.py
@@ -1,10 +1,48 @@
+import wx
 import json
 import urllib.request
 from time import sleep
+
+app = []; app = wx.App(None)
+dc = wx.ScreenDC()
+size = 275
+myFont = wx.Font(size, wx.DEFAULT, wx.NORMAL, wx.NORMAL, True)
+dc.SetFont(myFont)
+
+def main(string):
+    strings = [[""], [""], [""]]
+    splitstring = string.split(" ")
+    o = 0
+    added = 0
+    for i in splitstring:
+        if dc.GetTextExtent(strings[o][-1] + " " + i).x > 1280 and added > 0:
+            strings[o] += [" "]
+            o += 1
+            if o > 2:
+                o = 0
+            added = 0
+        if "\n" in i:
+            while o < 3 and (o != 0 or strings[o][-1] != " "):
+                strings[o] += [" "]
+                o += 1
+            o = 0
+            strings[o][-1] += i.split("\n")[1]
+            added = len(i.split("\n")[1])
+        else:
+            if strings[o][-1] != "":
+                strings[o][-1] += " " + i
+            else:
+                strings[o][-1] = i
+            added += len(i)
+    return strings
+
 while True:
     try:
         laststring = ""
         while True:
+            size = 275
+            myFont.SetPointSize(size)
+            dc.SetFont(myFont)
             contents = json.loads(urllib.request.urlopen("http://127.0.0.1:1025/v1/presentation/active").read())
             slides = []
             for i in contents["presentation"]["groups"]:
@@ -16,31 +54,14 @@ while True:
                 sleep(0.1)
                 continue
             laststring = string
-            strings = [[""], [""], [""]]
-            splitstring = string.split(" ")
-            o = 0
-            added = 0
-            for i in splitstring:
-                if added + len(i) > 7 and added > 0:
-                    strings[o] += [" "]
-                    o += 1
-                    if o > 2:
-                        o = 0
-                    added = 0
-                if "\n" in i:
-                    while o < 3 and (o != 0 or strings[o][-1] != " "):
-                        strings[o] += [" "]
-                        o += 1
-                    o = 0
-                    strings[o][-1] += i.split("\n")[1]
-                    added = len(i.split("\n")[1])
-                else:
-                    if strings[o][-1] != "":
-                        strings[o][-1] += " " + i
-                    else:
-                        strings[o][-1] = i
-                    added += len(i)
+            strings = main(string)
             lines = max(len(strings[0]), len(strings[1]), len(strings[2]))
+            while 2160 / dc.GetTextExtent("Test").y < lines:
+                size -= 1
+                myFont.SetPointSize(size)
+                dc.SetFont(myFont)
+                strings = main(string)
+                lines = max(len(strings[0]), len(strings[1]), len(strings[2]))
             for i in range(0, len(strings)):
                 while len(strings[i]) < lines:
                     strings[i] += [" "]
