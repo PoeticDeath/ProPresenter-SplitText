43a8cf09b00c651c1e3ff96df7b2b064e4835790
Anthony Kerr <46275713+PoeticDeath@users.noreply.github.com>
2024-08-11 20:01:40 -0400

    V1.1.0

--- SplitText.py
+++ SplitText.py
@@ -1,47 +1,44 @@
-import os
-import wx
 import json
 import tkinter as tk
 import urllib.request
 from time import sleep
+import tkinter.font as ft
 
-app = []; app = wx.App(None)
-dc = wx.ScreenDC()
 x = 3840
 y = 2160
 screens = 3
 maxsize = size = 250
-myFont = wx.Font(size, wx.DEFAULT, wx.NORMAL, wx.NORMAL, True)
-dc.SetFont(myFont)
+fontf = "Arial"
 
-if os.name != "nt":
-    root = tk.Tk()
-    root.geometry(f"{x}x{y}+5760+0")
-    root.config(bg = "#add")
-    root.wm_attributes("-transparentcolor", "#add")
-    root.overrideredirect(1)
+root = tk.Tk()
+root.config(bg = "systemTransparent")
+root.wm_attributes("-transparent", 1)
+root.overrideredirect(1)
 
-    T = []
-    for i in range(screens):
-        T += [tk.Text(root)]
-        T[i].config(bg = "#add", fg = "white", font = ("Arial", size), borderwidth = 0, wrap = "none")
-        T[i].pack()
-        T[i].place(x = (x // screens) * i, y = 0, width = x // screens, height = y)
-        Just = "center"
-        if i == 0:
-            Just = "right"
-        if i == screens - 1:
-            Just = "left"
-        T[i].tag_config("Just", justify = Just)
+font = ft.Font(font = (fontf, size))
+
+T = []
+for i in range(screens):
+    T += [tk.Text(root)]
+    T[i].config(bg = "systemTransparent", fg = "white", font = (fontf, size), borderwidth = 0, highlightthickness = 0, wrap = "none", state = "disabled")
+    T[i].pack()
+    T[i].place(x = (x // screens) * i, y = 0, width = x // screens, height = y)
+    Just = "center"
+    if i == 0:
+        Just = "right"
+    if i == screens - 1:
+        Just = "left"
+    T[i].tag_config("Just", justify = Just)
+
+root.geometry(f"{x}x{y}+5760+0")
 
 def main(string):
     strings = [[" "] for i in range(screens)]
     splitstring = string.split(" ")
     o = 0
     added = 0
-    maxadd = 0
     for i in splitstring:
-        if ((dc.GetTextExtent(strings[o][-1] + " " + i + " ").x > (x // screens) and added > 10) or (strings[o][-1].count(" ") > 2)) and strings[o][-1][-1] != "'":
+        if ((font.measure(strings[o][-1] + " " + i + " ") > (x // screens) and added > 10) or (strings[o][-1].count(" ") > 2)) and strings[o][-1][-1] != "'":
             strings[o] += [" "]
             o += 1
             if o > (screens - 1):
@@ -54,27 +51,20 @@ def main(string):
             o = 0
             strings[o][-1] += i.split("\n")[1]
             added = len(i.split("\n")[1])
-            sized = dc.GetTextExtent(strings[o][-1]).x
-            if sized > maxadd:
-                maxadd = sized
         else:
             if strings[o][-1] != " ":
                 strings[o][-1] += " " + i
             else:
                 strings[o][-1] = i
             added += len(i)
-            sized = dc.GetTextExtent(strings[o][-1]).x
-            if sized > maxadd:
-                maxadd = sized
-    return strings, (maxadd + dc.GetTextExtent("_").x - 1) // dc.GetTextExtent("_").x
+    return strings
 
 while True:
     try:
         laststring = ""
         while True:
             size = maxsize
-            myFont.SetPointSize(size)
-            dc.SetFont(myFont)
+            font = ft.Font(font = (fontf, size))
             contents = json.loads(urllib.request.urlopen("http://127.0.0.1:1025/v1/presentation/active").read())
             slides = []
             for i in contents["presentation"]["groups"]:
@@ -82,51 +72,45 @@ while True:
                     slides += [p["text"]]
             slideindex = json.loads(urllib.request.urlopen("http://127.0.0.1:1025/v1/presentation/slide_index").read())
             string = slides[slideindex["presentation_index"]["index"]].replace("\n", " \n")
-            for i in range(screens):
-                string = string.replace(f"From File Split{i + 1}.txt", "")
             if string == laststring:
-                if os.name != "nt":
-                    root.update()
+                root.update()
                 sleep(0.1)
                 continue
             laststring = string
-            strings, maxadd = main(string)
+            strings = main(string)
             for i in range(len(strings)):
                 while strings[i][-1] == " " and len(strings[i]) != 1:
                     strings[i].pop()
             lines = max(len(strings[0]), len(strings[1]), len(strings[2]))
-            while y / dc.GetTextExtent("Test").y < lines:
+            while y / font.metrics("linespace") < lines:
                 size -= 1
-                myFont.SetPointSize(size)
-                dc.SetFont(myFont)
-                strings, maxadd = main(string)
+                font = ft.Font(font = (fontf, size))
+                strings = main(string)
                 for i in range(len(strings)):
                     while strings[i][-1] == " " and len(strings[i]) != 1:
                         strings[i].pop()
                 lines = max(len(strings[0]), len(strings[1]), len(strings[2]))
-            while dc.GetTextExtent("_" * maxadd).x > x // screens:
+            while max([max([font.measure(o) for o in i]) for i in strings]) > x // screens:
                 size -= 1
-                myFont.SetPointSize(size)
-                dc.SetFont(myFont)
+                font = ft.Font(font = (fontf, size))
             for i in range(len(strings)):
                 while len(strings[i]) < lines:
                     strings[i] += [" "]
                 for p in range(len(strings[i])):
                     if strings[i][p] != " ":
                         strings[i][p] = strings[i][p].strip()
-                if os.name == "nt":
-                    strings[i] += ["_" * maxadd]
             if screens == 3:
                 for i in range(len(strings[1])):
                     if strings[1][i] == " ":
                         strings[0][i], strings[1][i] = strings[1][i], strings[0][i]
             for i in range(screens):
-                if os.name == "nt":
-                    one = open(f"Split{i + 1}.txt", "w").write("\n".join(strings[i]))
-                else:
-                    T[i].delete("1.0", tk.END)
-                    T[i].config(font = ("Arial", size))
-                    T[i].insert(tk.END, "\n".join(strings[i]), "Just")
+                root.wm_attributes("-transparent", 0)
+                T[i].config(state = "normal")
+                T[i].delete("1.0", tk.END)
+                T[i].config(font = (fontf, size))
+                T[i].insert(tk.END, "\n".join(strings[i]), "Just")
+                T[i].config(state = "disabled")
+                root.wm_attributes("-transparent", 1)
             sleep(0.1)
     except Exception:
         pass
